8d7cefa3d198ccc01b010761fece8de654b249c1
Pranay Pathole <38600100+PPathole@users.noreply.github.com>
2020-04-24 22:24:14 +0530

    Minor fixes on home.php

--- home.php
+++ home.php
@@ -154,10 +154,37 @@ fieldset {
 :-ms-input-placeholder {
   color: #888;
 }
+
+
+
+
   		</style>
 	</head>
 
 	<body>
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="home.php">Student Registration</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="login.php">Student Login</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <a href="admin.html">Admin Login</a>
+    </form>
+  </div>
+</nav>
 		<div class="container">  
   			<form id="contact" action="home.php" method="post">
     			<h3>Register here</h3>
@@ -213,7 +240,7 @@ fieldset {
 		if(mysqli_query($con, $sql)){
 			echo "<script type=\"text/javascript\">
 			alert('Successfully Registered!');
-			window.location='home.php'
+			window.location='login.php'
 			</script>";
 		}else{
 			echo "Error".$sql."<br>".$con->error;
